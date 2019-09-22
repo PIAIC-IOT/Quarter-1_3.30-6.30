479b2dca369b4e755b04c570ef4652b7ebb818bd
Muhammad Jawwad <47825998+jawwadturabi@users.noreply.github.com>
2019-09-23 00:15:42 +0500

    Update main.rs

--- week_08_15th-September,2019/class8-3/src/main.rs
+++ week_08_15th-September,2019/class8-3/src/main.rs
@@ -1,20 +1,5 @@
-fn main() {
-
-let user1 = build_user(String::from("uk@yaho"),String::from("ukasha"));
-
-println!("{:?}",user1);
-
-}
-
-fn build_user(email: String, username: String) -> User {
-    
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
-}
+////////////////////////
+//// making a struct name user and filling its fields using function  
 
 #[derive(Debug)]
 struct User {
@@ -22,4 +7,21 @@ struct User {
     email: String,
     sign_in_count: u64,
     active: bool,
+}
+
+fn main() {
+
+let user1 = build_user(String::from("uk@yaho"),String::from("ukasha"));// calling function
+
+    println!("{:#?}",user1);
+}
+
+fn build_user(email: String, username: String) -> User { //defining function with two parameters and return type is itself a struct
+    
+    User {
+        email:email,
+        username:username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
