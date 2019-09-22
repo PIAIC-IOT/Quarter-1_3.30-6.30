4ee4fcf02f31d19287fd4e3233d4edcb5058ab32
Muhammad Jawwad <47825998+jawwadturabi@users.noreply.github.com>
2019-09-23 00:10:30 +0500

    Update main.rs

--- week_08_15th-September,2019/class8-2/src/main.rs
+++ week_08_15th-September,2019/class8-2/src/main.rs
@@ -1,3 +1,6 @@
+///////////////////////
+/// making struct with name employee having different fields
+[#derive(Debug)]
 struct Employee {
    name:String,
    company:String,
@@ -10,15 +13,18 @@ fn main() {
       company:String::from("HBL"),
       age:50
    };
-   let emp2 = Employee {
-      name:String::from("Umer"),
-//     company:String::from("HBL"),
-     company:emp1.company,
-      age:50
+   let mut emp2 = Employee {
+      name:String::from("Younus"),
+      company:emp1.company,//using the company name of first instance
+      age:40
    };
 
- //Changing the value in the name field of a Employee instance  
-//   emp1.name =String::from("Hassan");
-//To get a specific value from the struct, we can use dot notation.
-   // println!("Name is :{} company is {} age is {}",emp1.name,emp1.company,emp1.age);
+ //Changing the value in the name field of a Employee instance. To change the field first we have to make an instance mutable
+  emp2.name =String::from("Hassan");
+   
+  println!("emp2 is : {:#?}",emp2);
+
+   
+   //To get a specific value from the struct, we can use dot notation.
+   println!("Name is :{} company is {} age is {}",emp1.name,emp1.company,emp1.age);
 }
