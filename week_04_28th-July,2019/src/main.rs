ca693bf6235dd0ad747b00339c5d30b744c67f89
ukasha <ukasha909@outlook.com>
2019-09-30 09:45:32 +0500

    week05 also added

---
+++ week_04_28th-July,2019/src/main.rs
@@ -0,0 +1,106 @@
+fn main() {
+
+    let mut number = 3;
+    
+    while number != 0 {
+        println!("{}!", number);
+        number = number - 1;
+        
+    }
+
+    println!("LIFTOFF!!!");
+    println!("end of loop");
+
+
+
+    // let fruits = ["apple", "mango",];
+
+    // // for index in fruits.iter(){
+    // //     println!("{}",index);
+    // // }
+    // println!("{:?}",fruits.iter());
+
+
+    // println!("hello world");
+
+    // for i in 0..10{
+    //     println!("{}",i);
+    // }
+
+    // println!("heyy");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let input = -5;
+
+
+    // if input == 0 || input<0{
+    //     println!("invalid value");
+    // } 
+    // else if input < 20{
+    //     println!("it is cold today");
+    // }
+    // else if input>=20 && input<=30{
+    //     println!("it is good day");
+    // }
+    // else{
+    //     println!("it is hot today");
+    // }
+
+    // 5 != 5
+
+    // and, or, not
+    // &&, ||, !
+    // true && true // true
+    // true && false // false
+    // false && true // false
+    // false && false // false
+
+    // true || true // true
+    // true || false // true
+    // false || true // true
+    // false || false // false
+
+
+
+
+}
+
+
+// //while loop
+
+// fn print_n_times(x: i32, y: String){
+    
+//     let mut z = 0;
+
+//     while z<x{
+//         println!("{}",y);
+//         z = z+1;
+//     }
+// }
+
+
+// //for loop
+
+// fn print_n_times(x: i32, y: String){
+    
+// for z in 0..x{
+//         println!("{}",y);
+//     }
+// }
+
+
