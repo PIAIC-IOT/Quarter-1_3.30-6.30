7562c79a6038887e89f2bc1750c0eee936c36637
ukasha <ukasha909@outlook.com>
2019-11-03 09:38:15 +0500

    added Q3

--- Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_3/src/main.rs
+++ Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_3/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum Vehicle{
+    Cars(String),
+    Truck(String),
+    Bikes(String),
+}
+fn main(){
+    let cars = Vehicle::Cars(String::from("Honda"));
+    let bikes = Vehicle::Bikes(String::from("Yamaha"));
+    let Truck = Vehicle::Truck(String::from("CAT"));
+
+    println!("{:?}" , cars);
+    println!("{:?}" , bikes);
+    println!("{:?}" , Truck);
+
 }
---
+++ Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_3/target/rls/debug/deps/Question_3-4ec9c26801e60c65.d
@@ -0,0 +1,5 @@
+/home/ukasha/Desktop/PIAIC-IoT-Repos/Quarter-1_3.30-6.30/Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_3/target/rls/debug/deps/Question_3-4ec9c26801e60c65.rmeta: src/main.rs
+
+/home/ukasha/Desktop/PIAIC-IoT-Repos/Quarter-1_3.30-6.30/Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_3/target/rls/debug/deps/Question_3-4ec9c26801e60c65.d: src/main.rs
+
+src/main.rs:
