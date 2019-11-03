9cd836fd30d91b474da2d1ea4a355c0044b2be06
ukasha <ukasha909@outlook.com>
2019-11-03 09:39:03 +0500

    added Q5

--- Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_5/src/main.rs
+++ Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_5/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let some_num1= Some(150);
+    let some_num2 = Some(614.98);
+    let some_string1 = Some(String::from("How are you?"));
+    let null_value:Option<f64> = None;
+    println!("{:?}" ,some_num1);
+    println!("{:?}" ,some_num2);
+    println!("{:?}" ,some_string1);
+    println!("{:?}" , null_value);
 }
---
+++ Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_5/target/rls/debug/deps/Question_5-b945edc6f3f9d2a7.d
@@ -0,0 +1,5 @@
+/home/ukasha/Desktop/PIAIC-IoT-Repos/Quarter-1_3.30-6.30/Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_5/target/rls/debug/deps/Question_5-b945edc6f3f9d2a7.rmeta: src/main.rs
+
+/home/ukasha/Desktop/PIAIC-IoT-Repos/Quarter-1_3.30-6.30/Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_5/target/rls/debug/deps/Question_5-b945edc6f3f9d2a7.d: src/main.rs
+
+src/main.rs:
