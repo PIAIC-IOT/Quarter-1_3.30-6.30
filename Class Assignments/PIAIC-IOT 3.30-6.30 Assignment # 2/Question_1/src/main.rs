c297916d6a2c5b903a1bc823d696ed998a267639
ukasha <ukasha909@outlook.com>
2019-11-03 09:15:38 +0500

    added Question1

--- Class Assignments/PIAIC-IOT 3.30-6.30 Assignment # 2/Question_1/src/main.rs
+++ Class Assignments/PIAIC-IOT 3.30-6.30 Assignment # 2/Question_1/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
+
+    // Because x belongs to non-primitive data type which stores in 
+    // Heap and in Rust we cannot create a shallow copy of non-primitive 
+    // data type.
 }
