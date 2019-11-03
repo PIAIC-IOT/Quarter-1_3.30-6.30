2213345be8a93693ea1463e44e7bae2f3de7f2a6
ukasha <ukasha909@outlook.com>
2019-11-03 09:39:32 +0500

    added Q6

--- Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_6/src/main.rs
+++ Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_6/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum Laptop {
+    HP,
+    Dell(Series),
+    Asus,
+    Lenovo,
+}
+#[derive(Debug)]
+enum Series{
+    S_1000,
+    S_2000,
+    S_3000,
+    S_4000,
+    S_5000,
+    S_6000,
+}
+
+fn main(){
+    let lap1 = Laptop::Dell(Series::S_1000);
+    let lap2 = Laptop::Dell(Series::S_6000);
+    println!("{:#?}\n{:#?}" , lap1 , lap2);
 }
