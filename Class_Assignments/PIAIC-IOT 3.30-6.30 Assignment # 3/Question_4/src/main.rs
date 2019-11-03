88d2a52638568a19b931f59236a1e3b0a5734f54
ukasha <ukasha909@outlook.com>
2019-11-03 09:38:37 +0500

    added Q4

--- Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_4/src/main.rs
+++ Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_4/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum Shape{
+    Circle(u32),
+    Triangle{a:u32,b:u32,c:u32},
+    Rectengle{a:u32,b:u32,c:u32,d:u32},
+    Square{a:u32,b:u32,c:u32,d:u32},
+}
+fn main(){
+    let shape1 = Shape::Circle(8);
+    let shape2 = Shape::Triangle{a:10 , b:20 , c: 30};
+    let shape3 = Shape::Rectengle{a:10 , b:20 , c: 30 , d:40 };
+    let shape4 = Shape::Square{a:10 , b:20 , c: 30 , d:40 };
+    shape1.call();
+    shape2.call();
+    shape3.call();
+    shape4.call();
+}
+
+impl Shape {
+    fn call(&self){
+        println!("Shape is {:?}", self);
+    }
 }
---
+++ Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_4/target/rls/debug/deps/Question_4-d9672a5c5759f3ec.d
@@ -0,0 +1,5 @@
+/home/ukasha/Desktop/PIAIC-IoT-Repos/Quarter-1_3.30-6.30/Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_4/target/rls/debug/deps/Question_4-d9672a5c5759f3ec.rmeta: src/main.rs
+
+/home/ukasha/Desktop/PIAIC-IoT-Repos/Quarter-1_3.30-6.30/Class_Assignments/PIAIC-IOT 3.30-6.30 Assignment # 3/Question_4/target/rls/debug/deps/Question_4-d9672a5c5759f3ec.d: src/main.rs
+
+src/main.rs:
